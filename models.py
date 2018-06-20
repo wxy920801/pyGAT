afddc5cc6c358da94e30093b2cea278e70006baa
nicole.wxy <nicole.wxy@alibaba-inc.com>
2018-06-20 16:13:14 +0800

    modify

--- models.py
+++ models.py
@@ -5,17 +5,20 @@ from layers import GraphAttentionLayer
 
 
 class GAT(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+    def __init__(self, nfeat, nhid, nhid1, nclass, dropout, alpha, nheads):
         super(GAT, self).__init__()
         self.dropout = dropout
 
         self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True) for _ in range(nheads)]
         for i, attention in enumerate(self.attentions):
             self.add_module('attention_{}'.format(i), attention)
+        
+        self.mid_att = GraphAttentionLayer(nhid * nheads, nhid1*nheads, dropout=dropout, alpha=alpha    , concat=False)
 
-        self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
-
+        self.out_att = GraphAttentionLayer(nhid1 * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
+        
     def forward(self, x, adj):
         x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
+        x = self.mid_att(x, adj)
         x = self.out_att(x, adj)
-        return F.log_softmax(x, dim=1)
+        return x
--- train.py
+++ train.py
@@ -20,7 +20,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
 parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
+parser.add_argument('--epochs', type=int, default=1, help='Number of epochs to train.')
 parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
 parser.add_argument('--hidden', type=int, default=8, help='Number of hidden units.')
@@ -41,10 +41,13 @@ if args.cuda:
 # Load data
 adj, features, labels, idx_train, idx_val, idx_test = load_data()
 
-# Model and optimizer
-model = GAT(nfeat=features.shape[1], nhid=args.hidden, nclass=int(labels.max()) + 1, dropout=args.dropout, nheads=args.nb_heads, alpha=args.alpha)
-optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+print(adj.shape)
+print(features.shape)
 
+
+# Model and optimizer
+model = GAT(nfeat=features.shape[1], nhid=args.hidden,nhid1=args.hidden, nclass=int(labels.max()) + 1, dropout=args.dropout, nheads=args.nb_heads, alpha=args.alpha)
+optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 if args.cuda:
     model.cuda()
     features = features.cuda()
@@ -62,6 +65,7 @@ def train(epoch):
     model.train()
     optimizer.zero_grad()
     output = model(features, adj)
+    print(output.shape)
     loss_train = F.nll_loss(output[idx_train], labels[idx_train])
     acc_train = accuracy(output[idx_train], labels[idx_train])
     loss_train.backward()
